128c2defddf79a7c2b2e78a0ae61ded2c6c2f20d
Nahuel Demarco <marco.denaju@gmail.com>
2023-09-27 15:02:48 -0300

    se agregó logo al navbar

--- src/components/layout/navbar/Navbar.jsx
+++ src/components/layout/navbar/Navbar.jsx
@@ -1,41 +1,42 @@
 // import "./Navbar.css"
 // // import logo from "./assets/images/logopb.png"
 
-// export const Navbar = ()=>{
-//     return (
-//     <nav className="navbar">
-//         {/* <img
-//         src={logo}
-//         alt="logo"
-//         /> */}
+export const Navbar = () => {
+  return (
+    <nav className="navbar">
+      {/* <img
+        src={logo}
+        alt="logo"
+        /> */}
 
-//         <img
-//         src="https://res.cloudinary.com/dnixufsko/image/upload/v1695750397/logopb_mcehwm.png"
-//         alt="logo"
-//         />
+      <img
+        src="https://res.cloudinary.com/dnixufsko/image/upload//c_thumb,w_80/v1695835129/231396019_304500691462675_5388614809897967354_n_q0u1f1.jpg"
+        alt="logo"
+      />
 
-//         <ul>
-//             <li>productos</li>
-//             <li>contacto</li>
-//             <li>inicio</li>
-//         </ul>
-//     </nav>
-//     );
-// }
+      <ul>
+        <li>Salsa picante</li>
+        <li>Salsa barbacoa</li>
+        <li>Salsa ketchup</li>
+        <li>Salsa agridulce</li>
+      </ul>
+    </nav>
+  );
+};
 
 // =======CLASE 4 COMPONENTES========
 
-export const Navbar = () => {
-  return (
-    <div>
-      <h4>Logo</h4>
-      <ul>
-        <li>Salsa 1</li>
-        <li>Salsa 2</li>
-        <li>Salsa 3</li>
-        <li>Salsa 4</li>
-      </ul>
-      {/* WIDGET CARRITO */}
-    </div>
-  );
-};
+// export const Navbar = () => {
+//   return (
+//     <div>
+//       <h4>Logo</h4>
+//       <ul>
+//         <li>Salsa 1</li>
+//         <li>Salsa 2</li>
+//         <li>Salsa 3</li>
+//         <li>Salsa 4</li>
+//       </ul>
+//       {/* WIDGET CARRITO */}
+//     </div>
+//   );
+// };
